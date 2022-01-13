058cf2fec955ee96b8b1277d334d0f684cb7ebe0
Starfosse <77751009+Starfosse@users.noreply.github.com>
2022-01-13 13:29:04 +0100

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouleau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/01 12:20:24 by abouleau          #+#    #+#             */
+/*   Updated: 2021/12/09 17:36:07 by abouleau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+size_t	ft_strlen(char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*get_next_line(int fd);
+char	*get_read(int fd, char *save);
+char	*get_line(char *save);
+char	*get_save(char *save);
+
+#endif
